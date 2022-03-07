e3fc06b8ede7c7723c125adff2350de79c8ac3b3
matouseek <matouszatloukal@seznam.cz>
2022-03-07 14:13:35 +0100

    Added text + input

--- main.py
+++ main.py
@@ -3,33 +3,93 @@ import sys as sus
 
 class Window:
     def __init__(self):
+
         tkinter.init()
         self.win = tkinter.display.set_mode((250, 550))
+
         self.gray = (220,220,220)
         self.black = (0,0,0)
+        self.red = (255, 0, 0)
+        self.green = (0, 255, 0)
+        self.purple = (75, 0, 130)
+
+        self.clock = tkinter.time.Clock()
+
         self.font = None
         self.text = None
         self.textRect = None
+
+        self.input_box = tkinter.Rect(5, 435, 140, 32)
+        self.color_inactive = (255, 0, 0)
+        self.color_active = (0, 255, 0)
+        self.color = self.color_inactive
+        self.active = False
+        self.textInput = ''
 
     def check_events(self):
         for event in tkinter.event.get():
             if event.type == tkinter.QUIT:
                 tkinter.quit()
                 sus.exit()
+            if event.type == tkinter.MOUSEBUTTONDOWN:
+                if self.input_box.collidepoint(event.pos):
+                    self.active = True
+                else:
+                    self.active = False
+
+                if self.active:
+                    self.color = self.color_active
+                else:
+                    self.color = self.color_inactive
+            
+            if event.type == tkinter.KEYDOWN:
+                if self.active:
+                    if event.key == tkinter.K_RETURN:
+                        print(self.textInput)
+                        self.textInput = ''
+                    elif event.key == tkinter.K_BACKSPACE:
+                        self.textInput = self.textInput[:-1]
+                    else:
+                        self.textInput += event.unicode
+                    
+                    if len(self.textInput) > 7:
+                        self.textInput = ''
 
     def loop(self):
         while True:
+            self.clock.tick(60)
             self.win.fill(self.gray)
+
+            tkinter.draw.rect(self.win, self.color, self.input_box, 2)
+            self.draw_text(self.textInput, 20, 75, 450, self.color)
+
             self.check_events()
 
             tkinter.draw.rect(self.win, (255, 50, 50), (0, 0, 245, 545), 4)
 
-            self.draw_text('transakce', 25, 150, 150, self.black)
+            self.draw_text('Smenarna', 25, 125, 25, self.black)
+
+            self.draw_text('Transakce', 23, 60, 50, self.black)
+            self.draw_text('Nakup', 20, 60, 75, self.red)
+            self.draw_text('Prodej', 20, 60, 105, self.red)
+
+            self.draw_text('Mena', 23, 60, 140, self.black)
+            self.draw_text('EUR', 20, 60, 165, self.red)
+            self.draw_text('GBP', 20, 60, 195, self.red)
+            self.draw_text('USD', 20, 60, 225, self.red)
+            self.draw_text('JPY', 20, 60, 255, self.red)
+            self.draw_text('IDR', 20, 60, 285, self.red)
+
+            self.draw_text('Kurz', 23, 60, 320, self.black)
+            self.draw_text('nejaky cislo', 20, 60, 345, self.purple)
+            self.draw_text('dalsi cislo', 20, 60, 375, self.purple)
+
+            self.draw_text('Vypocet', 23, 60, 410, self.black)
 
             tkinter.display.update()
     
     def draw_text(self, text, size, x, y, text_color):
-        self.font = tkinter.font.Font('lato', size)
+        self.font = tkinter.font.SysFont('notoserif', size)
         self.text = self.font.render(text, True, text_color)
         self.textRect = self.text.get_rect()
         self.textRect.center = (x, y)
