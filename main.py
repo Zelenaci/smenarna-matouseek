a5ba3fee60a36f4d6116f0ca34a1c69b81c49936
Matouš Zatloukal <matouszatloukal@seznam.cz>
2022-03-09 17:33:06 +0100

    Dodelany ukol

--- main.py
+++ main.py
@@ -26,6 +26,35 @@ class Window:
         self.active = False
         self.textInput = ''
 
+        self.nakup = None
+        self.prodej = None
+        self.eur = None
+        self.gbp = None
+        self.usd = None
+        self.jpy = None
+        self.cad = None
+
+        self.colorNakup = self.color_inactive
+        self.activeNakup = False
+        self.colorProdej = self.color_inactive
+        self.activeProdej = False
+
+        self.KolikKupujes = 1
+        self.ZaKolik = 1
+        self.KolikChci = 1
+        self.finalniCena = None
+
+        self.indexProdej = 3
+        self.indexNakup = 4
+        
+        self.listek = []
+
+        with open('listek.txt', 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                txt = line.split()
+                self.listek.append(txt)
+
     def check_events(self):
         for event in tkinter.event.get():
             if event.type == tkinter.QUIT:
@@ -41,18 +70,74 @@ class Window:
                     self.color = self.color_active
                 else:
                     self.color = self.color_inactive
+
+
+                if self.nakup.collidepoint(event.pos):
+                    self.activeNakup = True
+                    self.activeProdej = False
+                    self.colorNakup = self.color_active
+                    self.colorProdej = self.color_inactive
+
+
+                if self.prodej.collidepoint(event.pos):
+                    self.activeProdej = True
+                    self.activeNakup = False
+                    self.colorProdej = self.color_active
+                    self.colorNakup = self.color_inactive
+
+
+
+                if self.eur.collidepoint(event.pos):
+                    self.KolikKupujes = self.listek[0][1]
+                    self.KolikChci = 0
+                    if self.activeNakup:
+                        self.ZaKolik = self.listek[0][3]
+                    else:
+                        self.ZaKolik = self.listek[0][2]
+
+                if self.gbp.collidepoint(event.pos):
+                    self.KolikKupujes = self.listek[1][1]
+                    self.KolikChci = 0
+                    if self.activeNakup:
+                        self.ZaKolik = self.listek[1][3]
+                    else:
+                        self.ZaKolik = self.listek[1][2]
+
+                if self.usd.collidepoint(event.pos):
+                    self.KolikKupujes = self.listek[2][1]
+                    self.KolikChci = 0
+                    if self.activeNakup:
+                        self.ZaKolik = self.listek[2][3]
+                    else:
+                        self.ZaKolik = self.listek[2][2]
+
+                if self.jpy.collidepoint(event.pos):
+                    self.KolikKupujes = self.listek[3][1]
+                    self.KolikChci = 0
+                    if self.activeNakup:
+                        self.ZaKolik = self.listek[3][3]
+                    else:
+                        self.ZaKolik = self.listek[3][2]
+
+                if self.cad.collidepoint(event.pos):
+                    self.KolikKupujes = self.listek[4][1]
+                    self.KolikChci = 0
+                    if self.activeNakup:
+                        self.ZaKolik = self.listek[4][3]
+                    else:
+                        self.ZaKolik = self.listek[4][2]
             
             if event.type == tkinter.KEYDOWN:
                 if self.active:
                     if event.key == tkinter.K_RETURN:
-                        print(self.textInput)
+                        self.KolikChci = self.textInput
                         self.textInput = ''
                     elif event.key == tkinter.K_BACKSPACE:
                         self.textInput = self.textInput[:-1]
                     else:
                         self.textInput += event.unicode
                     
-                    if len(self.textInput) > 7:
+                    if len(self.textInput) > 9:
                         self.textInput = ''
 
     def loop(self):
@@ -70,21 +155,28 @@ class Window:
             self.draw_text('Smenarna', 25, 125, 25, self.black)
 
             self.draw_text('Transakce', 23, 60, 50, self.black)
-            self.draw_text('Nakup', 20, 60, 75, self.red)
-            self.draw_text('Prodej', 20, 60, 105, self.red)
+            self.nakup = self.draw_text('Nakup', 20, 60, 75, self.colorNakup)
+            self.prodej = self.draw_text('Prodej', 20, 60, 105, self.colorProdej)
 
             self.draw_text('Mena', 23, 60, 140, self.black)
-            self.draw_text('EUR', 20, 60, 165, self.red)
-            self.draw_text('GBP', 20, 60, 195, self.red)
-            self.draw_text('USD', 20, 60, 225, self.red)
-            self.draw_text('JPY', 20, 60, 255, self.red)
-            self.draw_text('IDR', 20, 60, 285, self.red)
+            self.eur = self.draw_text('EUR', 20, 60, 165, self.red)
+            self.gbp = self.draw_text('GBP', 20, 60, 195, self.red)
+            self.usd = self.draw_text('USD', 20, 60, 225, self.red)
+            self.jpy = self.draw_text('JPY', 20, 60, 255, self.red)
+            self.cad = self.draw_text('CAD', 20, 60, 285, self.red)
 
             self.draw_text('Kurz', 23, 60, 320, self.black)
-            self.draw_text('nejaky cislo', 20, 60, 345, self.purple)
-            self.draw_text('dalsi cislo', 20, 60, 375, self.purple)
+            self.draw_text(str(self.KolikKupujes), 20, 60, 345, self.purple)
+            self.draw_text(str(self.ZaKolik), 20, 60, 375, self.purple)
 
             self.draw_text('Vypocet', 23, 60, 410, self.black)
+
+            try:
+                self.finalniCena = (float(self.KolikChci)/float(self.KolikKupujes))*float(self.ZaKolik)
+            except:
+                self.finalniCena = 'Nevalidni hodnota'
+
+            self.draw_text(str(self.finalniCena), 20, 60, 480, self.purple)
 
             tkinter.display.update()
     
